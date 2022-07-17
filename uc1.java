d1ba9373284bcf2e5cbb573bc7fa96659554cea7
Anusha <anushavysakh232@gmail.com>
2022-07-18 01:22:01 +0530

    Committed u4

--- uc1.java
+++ uc1.java
@@ -1,28 +1,21 @@
 package com.Assignment.Bridlabz;
 
-public class uc3 {
+public class uc4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int workingHour;
-		int wagePerHour=20;
 		int attendence= (int) (Math.floor(Math.random()*10))%3;
-	
-		if(attendence == 0) {
-			System.out.println("Employee Absent..");
-		workingHour=0;
-		} else
-		if(attendence==1){
-			System.out.println("Employee Present and full-time employee");
-			workingHour=8;
-		}else {
-			System.out.println("Employee Present and part-time employee");
-			workingHour=4;
-		}
-		int Dailywage=wagePerHour*workingHour;
-		System.out.println("Daily wage of empoyee= " +Dailywage);
-		}
-	
+		switch(attendence) {
+			case 0:
+					System.out.println("Employee Absent");
+					break;
+			case 1:
+					System.out.println("Employee Full-Time");
+					break;
+			default:
+					System.out.println("Employee Part-Time");
+					break;
+			}
 	}
 
-
+}
