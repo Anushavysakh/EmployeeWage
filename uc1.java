e8a8cd13d3a26fa0c9b5d1bf3ff1547354153ac6
Anusha <anushavysakh232@gmail.com>
2022-07-18 01:17:54 +0530

    Committed u2

--- uc1.java
+++ uc1.java
@@ -1,19 +1,22 @@
 package com.Assignment.Bridlabz;
-//import java.util.Random;
-public class uc1 {
+
+public class uc2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 int attendence= (int) (Math.floor(Math.random()*10))%2;
-		 System.out.println("Welcome to Employee Wage Computation Program");
-
-		//	Random random= new Random();
-		//	int attendance=random.nextInt(2);
-
-			if(attendence == 1)
+	
+			int workingHour;
+			int wagePerHour=20;
+			int attendence= (int) (Math.floor(Math.random()*10))%2;
+		
+			if(attendence == 1) {
 				System.out.println("Employee Present..");
-			else
+			workingHour=8;
+			} else {
 				System.out.println("Employee Absent..");
-	}
+				workingHour=0;
+			}
+			int Dailywage=wagePerHour*workingHour;
+			System.out.println("Daily wage of empoyee= " +Dailywage);
+			}
 
 }
